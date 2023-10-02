2e8bb69918385773234b428c4a95747c4f13f62e
Noguchi-Kanta <a21ja115@st.osaka-cu.ac.jp>
2023-10-02 13:14:56 +0900

    投稿作成画面

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,23 +9,26 @@
         <form action="/posts" method="POST">
             @csrf
             <div class="title">
-                <h2>Title</h2>
+                <h2>タイトル</h2>
                 <input type="text" name="post[title]" placeholder="タイトル"/>
             </div>
             <div class="body">
-                <h2>Body</h2>
+                <h2>本文</h2>
                 <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
             </div>
             <div class="image">
+                <h2>写真</h2>
                 <input type="file" name="image">
             </div>
             <div>
                 <h2>タグ</h2>
-                <select name="post[tag_id]">
-                    @foreach($tags as $tag)
-                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                    @endforeach
-                </select>
+                @foreach($tags as $tag)
+                    <label>
+                    <input type="checkbox" value="{{ $tag->id }}" name="tag_array[]">
+                        {{$tag->tag_name}}
+                    </input>
+                    </label>
+                @endforeach
             </div>
             <input type="submit" value="store"/>
         </form>
