cab1a1be4dbd77d0cabb74c7caccf739870167f0
Noguchi-Kanta <a21ja115@st.osaka-cu.ac.jp>
2023-10-14 15:48:14 +0900

    migration修正

--- database/migrations/2023_09_11_104653_create_posts_table.php
+++ database/migrations/2023_09_11_104653_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            //$table->unsignedBigInteger('user_id');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title', 50);
             $table->string('body', 200);
